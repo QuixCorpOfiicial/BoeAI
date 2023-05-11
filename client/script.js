15b8a450bbb0ae91d6d3b0222dbabf99296a75d1
Quix Corperation Community <quixcorpofficial@gmail.com>
2023-05-11 15:52:08 +0530

    updated minor fix

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch('https://boeai.onrender.com', {
+    const response = await fetch('http://localhost:8000', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
